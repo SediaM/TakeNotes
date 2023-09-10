bf4034b45a37a9528629ad2e52c4ebf33f6bcaa8
SediaM <sediamassaquoi@gmail.com>
2023-09-10 16:15:20 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const path = require('path');
 const uuid = require('./helpers/uuid');
-const {
-  readFromFile,
-  readAndAppend,
-  writeToFile,
-} = require('./helpers/fsUtils');
 
 const app = express();
 const dbData = require('./db/db.json');
@@ -25,37 +20,32 @@ app.get('/api/notes', (req, res) => {
 
 // POST request for reviews
 app.post('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
   console.log(req.body);
 
-  const { title, text } = req.body;
-
-  if (req.body) {
-    const newNote = {
-      title,
-      text,
-    };
-
-    readAndAppend(newNote, './db/db.json');
-    res.json(`Note added successfully`);
-  } else {
-    res.error('Error in adding note');
-  }
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuid,
+  };
+  notes.push(newNote);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  res.json(notes);
 });
 
+
 app.delete('/api/notes/:id', (req, res) => {
-  const noteId = req.params.note_id;
-  readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-      // Make a new array of all tips except the one with the ID provided in the URL
-      const result = json.filter((note) => note.note_id !== noteId);
+  const noteId = req.params.id;
+  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+  // Make a new array of all tips except the one with the ID provided in the URL
+  const result = notes.filter((note) => { return note.id !== noteId });
 
-      // Save that array to the filesystem
-      writeToFile('./db/db.json', result);
+  // Save that array to the filesystem
+  fs.writeFileSync('./db/db.json', JSON.stringify(result));
 
-      // Respond to the DELETE request
-      res.json(`Item ${noteId} has been deleted 🗑️`);
-    });
+  // Respond to the DELETE request
+  res.json(result, `Note has been deleted 🗑️`);
+
 });
 
 app.get('/notes', (req, res) =>
